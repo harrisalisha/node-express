90fac381a15ff9518bb9baed550642a13ed1c17e
harrishalisha <noranalisha@gmail.com>
2022-03-11 11:45:50 +0000

    Express Serve Static Files

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 
@@ -7,8 +8,12 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 //middleware
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));//default serve public/index.html
+
 app.use((req, res, next) => {
-    console.log(req.headers);
+    //console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
